3cfe1768df76c05c29118ccfdc3f67026c7ad464
Aadetri <aadetritawara@gmail.com>
2026-05-11 23:19:27 -0700

    Enhance Lambda function to publish job status to Redis

--- services/birdnet_processor/lambda_handler.py
+++ services/birdnet_processor/lambda_handler.py
@@ -7,6 +7,7 @@ try:
 except ImportError:
     pass
 
+import redis
 import os
 import boto3
 import json
@@ -19,11 +20,13 @@ from datetime import datetime
 
 db_url = os.environ.get("DATABASE_URL") # lambda environment variable
 
+r = redis.Redis(host=os.environ.get("REDIS_HOST"), port=6379, decode_responses=True)
+
 # initialize database connection outside handler in case of warm starts
 connection = None
 try:
     print("Attempting to connect to RDS...")
-    connection = psycopg2.connect(db_url)
+    connection = psycopg2.connect(db_url, cursor_factory=RealDictCursor)
     print("Database connection successful.")
 except Exception as e:
     print(f"Database connection failed: {e}")
@@ -39,18 +42,20 @@ print("Model loaded.")
 def lambda_handler(event, context): 
     
     global connection
+    global r
+
     tmp_file_path = None
 
     # reconnect if connection dropped
     if connection.closed != 0:
-        connection = psycopg2.connect(db_url)
+        connection = psycopg2.connect(db_url, cursor_factory=RealDictCursor)
 
     # download audio file from s3 bucket
     try: 
         bucket = event['Records'][0]['s3']['bucket']['name']
         key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
 
-        current_job_id = int(key.split('/')[-1].split('.')[0]) 
+        current_job_id = int(key.split('/')[-1].split('.')[0]) # assumes S3 key format is always "s3://<bucket>/<job_id>.wav"
 
         # download audio file to temporary storage
         tmp_file_path = f"/tmp/{key.split('/')[-1]}"
@@ -77,9 +82,9 @@ def lambda_handler(event, context):
                 raise Exception(f"Job ID {current_job_id} not found in database.")
 
             # grab lat, lon, and date from database
-            user_lat = select_result[0]
-            user_lon = select_result[1]
-            submitted_date = select_result[2]
+            user_lat = select_result['latitude']
+            user_lon = select_result['longitude']
+            submitted_date = select_result['created_at']
 
             recording = Recording(
                 analyzer,
@@ -109,6 +114,22 @@ def lambda_handler(event, context):
 
             connection.commit()
             print("Successfully saved all detections to RDS.")
+
+            try:
+                # publish to Redis stream for SSE updates
+                r.publish(f"job:{current_job_id}", json.dumps({ 
+                    "status": "classified",
+                    "message": "Lambda Processing complete."
+                 })) 
+            except (redis.exceptions.ConnectionError, redis.exceptions.TimeoutError):
+                print("Redis connection lost. Reconnecting...")
+                r = redis.Redis(host=os.environ.get("REDIS_HOST"), port=6379, decode_responses=True)
+
+                # try publishing again after reconnecting
+                r.publish(f"job:{current_job_id}", json.dumps({ 
+                    "status": "classified",
+                    "message": "Lambda Processing complete."
+                 })) 
 
     except Exception as e:
         print(f"Error processing {key}: {e}")
