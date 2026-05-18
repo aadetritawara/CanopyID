86ab7919c9d4ae18bb1cdbdc82a897e71abb764e
Aadetri <aadetritawara@gmail.com>
2026-05-18 16:08:27 -0700

    Refactor database configuration to include POSTGRES_HOST and update DB_URL format

--- backend/config.py
+++ backend/config.py
@@ -2,13 +2,21 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
 
+    # Database settings
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
+    POSTGRES_HOST: str
+
+    # LangChain settings
+    GROQ_API_KEY: str
+    LANGSMITH_API_KEY: str
+    LANGSMITH_TRACING: bool
 
     model_config = SettingsConfigDict(
         env_file=".env",
-        env_file_encoding="utf-8"        
+        env_file_encoding="utf-8",
+        extra="ignore"     
     )
 
 settings = Settings()
--- backend/db/database.py
+++ backend/db/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 from typing import AsyncGenerator
-import os
 from backend.config import settings
 
-DB_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@db/{settings.POSTGRES_DB}"
+DB_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}/{settings.POSTGRES_DB}"
 
 engine = create_async_engine(DB_URL, echo=False) 
 
